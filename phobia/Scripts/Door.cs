dad465234d9e4ac09ec10a571dc22aabf4fd5fe0
Mia Miranda <miamiranda50@gmail.com>
2025-03-08 20:38:34 -0600

    Main Menu Music

---
+++ phobia/Scripts/Door.cs
@@ -0,0 +1,81 @@
+using Godot;
+using System;
+
+
+/// <summary>
+/// The Door class takes an animation player and plays an animation when the player interacts with it.
+/// </summary>
+public partial class Door : CsgBox3D
+{
+	private bool toggle = false;
+	private bool interactable = true;
+	private AudioStreamPlayer3D doorCloseSound;
+	private AudioStreamPlayer3D doorOpenSound;
+	
+
+	[Export]
+	private AnimationPlayer doorAnimPlayer {get; set; }
+
+	[Export]
+
+	private Timer doorTimer {get; set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		doorCloseSound = GetNode<AudioStreamPlayer3D>("DoorCloseSFX");
+		doorOpenSound = GetNode<AudioStreamPlayer3D>("DoorOpenSFX");
+
+		doorAnimPlayer.AnimationFinished += OnAnimFinished;
+		doorAnimPlayer.AnimationStarted += OnAnimStarted;
+		doorTimer.Timeout += OnDoorTimerTimeout;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	/// <summary>
+	/// Interact() Allows the player to interact with the door. Plays an animation on close and open.
+	/// </summary>
+	public void Interact()
+	{
+		if (interactable == true)
+		{
+			interactable = false;
+			toggle = !toggle;
+			if (toggle == false)
+			{
+				doorAnimPlayer.Play("close");
+			}
+			if (toggle == true)
+			{
+				doorAnimPlayer.Play("open");
+			}
+			
+			doorTimer.Start();
+		}
+	}
+
+	private void OnAnimFinished(StringName anim)
+	{
+		if(anim == "close")
+		{
+			doorCloseSound.Play();
+		}
+	}
+
+	private void OnAnimStarted(StringName anim)
+	{
+		if(anim == "open")
+		{
+			doorOpenSound.Play();
+		}
+	}
+
+	private void OnDoorTimerTimeout()
+	{
+		interactable = true;
+	}
+}
